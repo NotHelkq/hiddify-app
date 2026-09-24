71dbf8191f4549f4eed618765b3d0167d5b126f2
NotHelkq <nothelkq@users.noreply.github.com>
2026-09-24 15:43:30 +0400

    bump: version 4.1.6+40106 (singbox detour fix)

--- lib/core/model/app_info_entity.dart
+++ lib/core/model/app_info_entity.dart
@@ -20,7 +20,7 @@ class AppInfoEntity with _$AppInfoEntity {
   String get userAgent => "HiddifyNext/$version ($operatingSystem) like ClashMeta v2ray sing-box";
 
   String get presentVersion =>
-      environment == Environment.prod ? "$version (deep log & fgs fix)" : "$version ${environment.name} (deep log & fgs fix)";
+      environment == Environment.prod ? "$version (singbox detour fix)" : "$version ${environment.name} (singbox detour fix)";
 
   /// formats app info for sharing
   String format() =>
